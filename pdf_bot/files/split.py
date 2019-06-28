e525dd5480968eed825542338fb1e9910e88c09d
Joshua <joshuaystang@gmail.com>
2019-06-29 00:36:44 +1000

    Tidied up split functions

--- pdf_bot/files/split.py
+++ pdf_bot/files/split.py
@@ -21,9 +21,10 @@ def ask_split_range(update, _):
     Returns:
         The variable indicating to wait for the split page range
     """
-    update.message.reply_text('Please send me the range of pages that you will like to keep. You can use ⚡ *INSTANT '
-                              'VIEW* from below or refer to [here](http://telegra.ph/Telegram-PDF-Bot-07-16) for '
-                              'some range examples.', parse_mode='markdown', reply_markup=ReplyKeyboardRemove())
+    update.message.reply_text('Send me the range of pages that you\'ll like to keep. '
+                              'You can use ⚡ *INSTANT VIEW* from below or '
+                              'refer to [here](http://telegra.ph/Telegram-PDF-Bot-07-16) for some range examples.',
+                              parse_mode='markdown', reply_markup=ReplyKeyboardRemove())
 
     return WAIT_SPLIT_RANGE
 
@@ -45,32 +46,26 @@ def split_pdf(update, context):
 
     split_range = update.message.text
     if not PageRange.valid(split_range):
-        update.message.reply_text('The range is invalid. Please send me the range again.')
+        update.message.reply_text('The range is invalid. Try again.')
 
         return WAIT_SPLIT_RANGE
 
     update.message.reply_text('Splitting your PDF file')
 
-    # Setup temporary files
-    temp_files = [tempfile.NamedTemporaryFile(), tempfile.NamedTemporaryFile(prefix='Split_', suffix='.pdf')]
-    file_name, out_file_name = [x.name for x in temp_files]
+    with tempfile.NamedTemporaryFile()  as tf:
+        # Download PDF file
+        file_id, file_name = user_data[PDF_INFO]
+        pdf_file = context.bot.get_file(file_id)
+        pdf_file.download(custom_path=tf.name)
+        pdf_reader = open_pdf(tf.name, update)
 
-    # Download PDF file
-    file_id = user_data[PDF_INFO]
-    pdf_file = context.bot.get_file(file_id)
-    pdf_file.download(custom_path=file_name)
-    pdf_reader = open_pdf(file_name, update)
+        if pdf_reader is not None:
+            merger = PdfFileMerger()
+            merger.append(pdf_reader, pages=PageRange(split_range))
+            send_result(update, merger, file_name, 'split')
 
-    if pdf_reader is not None:
-        merger = PdfFileMerger()
-        merger.append(pdf_reader, pages=PageRange(split_range))
-        merger.write(out_file_name)
-        send_result(update, out_file_name, 'split')
-
-    # Clean up memory and files
+    # Clean up memory
     if user_data[PDF_INFO] == file_id:
         del user_data[PDF_INFO]
-    for tf in temp_files:
-        tf.close()
 
     return ConversationHandler.END
