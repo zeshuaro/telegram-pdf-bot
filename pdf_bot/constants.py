2974f9a74dd344b8c7b7140c99ac8ab4b4857235
Joshua <joshuaystang@gmail.com>
2019-07-01 23:07:16 +1000

    Tidied up constants

--- pdf_bot/constants.py
+++ pdf_bot/constants.py
@@ -1,6 +1,19 @@
+# Bot information
 BOT_NAME = 'pdf2bot'
 CHANNEL_NAME = 'pdf2botdev'
 
+# Compare constants
+WAIT_COMPARE_FIRST = 0
+WAIT_COMPARE_SECOND = 1
+
+# Merge constants
+WAIT_MERGE = 0
+
+# Watermark constants
+WAIT_WATERMARK_SOURCE = 0
+WAIT_WATERMARK = 1
+
+# File constants
 WAIT_TASK = 0
 WAIT_DECRYPT_PW = 1
 WAIT_ENCRYPT_PW = 2
@@ -15,23 +28,21 @@ WAIT_CROP_TYPE = 10
 WAIT_CROP_PERCENT = 11
 WAIT_CROP_OFFSET = 12
 
-WAIT_COMPARE_FIRST = 0
-WAIT_COMPARE_SECOND = 1
-
-WAIT_MERGE = 0
-
+# Photo constants
 WAIT_PHOTO = 0
 
-WAIT_WATERMARK_SOURCE = 0
-WAIT_WATERMARK = 1
-
+# PDF file validation constants
 PDF_OK = 0
 PDF_INVALID_FORMAT = 1
 PDF_TOO_LARGE = 2
 
+# Keyboard Constants
 CANCEL = 'cancel'
+
+# User data constants
 PDF_INFO = 'pdf_info'
 
+# Payment Constants
 PAYMENT = 'payment'
 PAYMENT_PAYLOAD = 'payment_payload'
 PAYMENT_CURRENCY = 'USD'
@@ -41,6 +52,5 @@ PAYMENT_COFFEE = 'Coffee ☕ ($3)'
 PAYMENT_BEER = 'Beer 🍺 ($5)'
 PAYMENT_MEAL = 'Meal 🍲 ($10)'
 PAYMENT_CUSTOM = 'Say Awesome 🤩 (Custom)'
-WAIT_PAYMENT = 0
-
 PAYMENT_DICT = {PAYMENT_THANKS: 1, PAYMENT_COFFEE: 3, PAYMENT_BEER: 5, PAYMENT_MEAL: 10}
+WAIT_PAYMENT = 0
