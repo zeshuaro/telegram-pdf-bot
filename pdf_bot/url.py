f9d2fa3624b081529112a942cf41f75a07c4d6ad
Joshua <joshuaystang@gmail.com>
2019-07-02 21:54:20 +1000

    Added to check if url is reachable

--- pdf_bot/url.py
+++ pdf_bot/url.py
@@ -5,6 +5,7 @@ import tempfile
 from telegram.ext.dispatcher import run_async
 from urllib.parse import urlparse
 from weasyprint import HTML
+from weasyprint.urls import URLFetchingError
 
 from pdf_bot.utils import send_result_file
 
@@ -18,9 +19,9 @@ def url_to_pdf(update, context):
     user_data = context.user_data
 
     if URLS in user_data and url in user_data[URLS]:
-        update.message.reply_text('You\'ve sent me this web page already and I\'m still converting it')
+        update.message.reply_text('You\'ve sent me this web page already and I\'m still converting it.')
     else:
-        update.message.reply_text('Converting your web page into a PDF file')
+        update.message.reply_text('Converting your web page into a PDF file.')
         if URLS in user_data:
             user_data[URLS].add(url)
         else:
@@ -28,8 +29,10 @@ def url_to_pdf(update, context):
 
         with tempfile.TemporaryDirectory() as dir_name:
             out_fn = os.path.join(dir_name, f'{urlparse(url).netloc}.pdf')
-            HTML(url=url).write_pdf(out_fn)
-
-            send_result_file(update, out_fn)
+            try:
+                HTML(url=url).write_pdf(out_fn)
+                send_result_file(update, out_fn)
+            except URLFetchingError:
+                update.message.reply_text('Unable to reach your web page.')
 
         user_data[URLS].remove(url)
