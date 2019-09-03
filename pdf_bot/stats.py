253fdd8743885f665ede7288f71667c2a11028b0
Joshua <joshuaystang@gmail.com>
2019-09-04 00:02:48 +1000

    Added to produce a plot when getting stats

--- pdf_bot/stats.py
+++ pdf_bot/stats.py
@@ -1,8 +1,15 @@
+import matplotlib
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+import tempfile
+
+from collections import defaultdict
 from datetime import date
 from google.cloud import datastore
 
 from pdf_bot.store import client
-from pdf_bot.constants import USER
+from pdf_bot.constants import USER, LANGUAGE
 
 
 def update_stats(update, task):
@@ -24,10 +31,15 @@ def update_stats(update, task):
 def get_stats(update, context):
     query = client.query(kind=USER)
     num_users = num_tasks = 0
+    counts = defaultdict(int)
 
     for user in query.fetch():
         num_users += 1
-        num_tasks += sum([x for x in user.values() if isinstance(x, int)])
+        for key in user.keys():
+            if key != LANGUAGE:
+                num_tasks += user[key]
+                if key != 'count':
+                    counts[key] += user[key]
 
     launch_date = date(2017, 7, 1)
     stats_date = date(2019, 7, 1)
@@ -39,3 +51,23 @@ def get_stats(update, context):
 
     update.effective_message.reply_text(
         f'Total users: {num_users}\nTotal tasks: {num_tasks}\nEstimated total tasks: {est_num_tasks}')
+    send_plot(update, counts)
+
+
+def send_plot(update, counts):
+    tasks = sorted(counts.keys())
+    nums = [counts[x] for x in tasks]
+    x_pos = list(range(len(tasks)))
+
+    plt.rcdefaults()
+    fig, ax = plt.subplots()
+
+    ax.bar(x_pos, nums, align='center')
+    ax.set_xticks(x_pos)
+    ax.set_xticklabels(tasks)
+    ax.set_xlabel('Tasks')
+    ax.set_ylabel('Counts')
+
+    with tempfile.NamedTemporaryFile(suffix='.png') as tf:
+        plt.savefig(tf.name)
+        update.effective_message.reply_photo(open(tf.name, 'rb'))
