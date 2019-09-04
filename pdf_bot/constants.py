8ee9ae23f34666e124313c86d290c5e9ec64f7a5
Joshua <joshuaystang@gmail.com>
2019-09-05 00:25:23 +1000

    Fixed language

--- pdf_bot/constants.py
+++ pdf_bot/constants.py
@@ -1,6 +1,6 @@
 import gettext
 
-t = gettext.translation('pdf_bot', localedir='locale', languages=['en_UK'])
+t = gettext.translation('pdf_bot', localedir='locale', languages=['en'])
 _ = t.gettext
 
 # Bot constants
