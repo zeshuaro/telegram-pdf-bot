c633bc568b74cd5834ddb61f45d7c3411f0a56f0
Joshua <joshuaystang@gmail.com>
2019-09-05 20:57:14 +1000

    Improve the check for number of files that the user has sent through

--- pdf_bot/commands/merge.py
+++ pdf_bot/commands/merge.py
@@ -38,10 +38,14 @@ def merge(update, context):
     user_data[MERGE_IDS] = []
     user_data[MERGE_NAMES] = []
 
+    return ask_first_doc(update, context)
+
+
+def ask_first_doc(update, context):
     _ = set_lang(update, context)
     reply_markup = ReplyKeyboardMarkup([[_(CANCEL)]], resize_keyboard=True, one_time_keyboard=True)
     update.effective_message.reply_text(_(
-        'Send me the PDF file that you\'ll like to merge\n\n'
+        'Send me the first PDF file that you\'ll like to merge\n\n'
         'Note that the files will be merged in the order that you send me'),
         reply_markup=reply_markup)
 
@@ -52,7 +56,7 @@ def merge(update, context):
 def check_doc(update, context):
     result = check_pdf(update, context, send_msg=False)
     if result in [PDF_INVALID_FORMAT, PDF_TOO_LARGE]:
-        return process_invalid_pdf(update, context)
+        return process_invalid_pdf(update, context, result)
 
     # Append the file details
     context.user_data[MERGE_IDS].append(update.effective_message.document.file_id)
@@ -61,26 +65,18 @@ def check_doc(update, context):
     return ask_next_doc(update, context)
 
 
-def process_invalid_pdf(update, context):
+def process_invalid_pdf(update, context, result):
     _ = set_lang(update, context)
-    message = update.effective_message
-    result = check_pdf(update, context, send_msg=False)
-
     if result == PDF_INVALID_FORMAT:
-        text = _('The file you\'ve sent is not a PDF file\n\n')
+        text = _('The file you\'ve sent is not a PDF file')
     else:
-        text = _('The PDF file you\'ve sent is too large for me to download\n\n')
+        text = _('The PDF file you\'ve sent is too large for me to download')
 
-    # Check if user has already sent through some PDF files
-    if context.user_data[MERGE_NAMES]:
-        text += _('You can continue merging with the files that you\'ve sent me')
-        message.reply_text(text)
-        send_file_names(update, context, context.user_data[MERGE_NAMES], _('PDF files'))
+    update.effective_message.reply_text(text)
+    if not context.user_data[MERGE_NAMES]:
+        return ask_first_doc(update, context)
     else:
-        text += _('Send me the PDF file that you\'ll like to merge\n\n')
-        message.reply_text(text)
-
-    return WAIT_MERGE
+        return ask_next_doc(update, context)
 
 
 def ask_next_doc(update, context):
@@ -98,32 +94,37 @@ def ask_next_doc(update, context):
 @run_async
 def check_text(update, context):
     _ = set_lang(update, context)
-    text = update.effective_message.text
+    message = update.effective_message
+    text = message.text
 
     if text == _(DONE):
-        return merge_pdf(update, context)
+        if not check_user_data(update, context, MERGE_IDS):
+            return ConversationHandler.END
+
+        num_files = len(context.user_data[MERGE_IDS])
+        if num_files == 0:
+            message.reply_text(_('You haven\'t sent me any PDF files'))
+
+            return ask_first_doc(update, context)
+        elif num_files == 1:
+            message.reply_text(_('You\'ve only sent me one PDF file.'))
+
+            return ask_next_doc(update, context)
+        else:
+            return merge_pdf(update, context)
     elif text == _(CANCEL):
         return cancel_without_async(update, context)
 
 
 def merge_pdf(update, context):
-    if not check_user_data(update, context, MERGE_IDS):
-        return ConversationHandler.END
-
     _ = set_lang(update, context)
+    update.effective_message.reply_text(_(
+        'Merging your PDF files'), reply_markup=ReplyKeyboardRemove())
+
+    # Setup temporary files
     user_data = context.user_data
     file_ids = user_data[MERGE_IDS]
     file_names = user_data[MERGE_NAMES]
-
-    if len(file_ids) == 1:
-        update.effective_message.reply_text(_('You\'ve only sent me one PDF file.'))
-
-        return ask_next_doc(update, context)
-
-    update.effective_message.reply_text(_('Merging your PDF files'),
-                                        reply_markup=ReplyKeyboardRemove())
-
-    # Setup temporary files
     temp_files = [tempfile.NamedTemporaryFile() for _ in range(len(file_ids))]
     merger = PdfFileMerger()
 
