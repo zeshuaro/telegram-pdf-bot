ceb2f6680f14573fc4f6f2e5e1ab3e67e6ed59e2
Joshua <joshuaystang@gmail.com>
2021-01-03 21:04:22 +1100

    Fix convert photo to work with batched photos

--- pdf_bot/commands/photo.py
+++ pdf_bot/commands/photo.py
@@ -3,9 +3,24 @@ import noteshrink
 import os
 import tempfile
 
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, ParseMode
+from collections import defaultdict
+from telegram import (
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove,
+    ParseMode,
+    Update,
+    ChatAction,
+)
 from telegram.constants import MAX_FILESIZE_DOWNLOAD
-from telegram.ext import ConversationHandler, CommandHandler, MessageHandler, Filters
+from telegram.ext import (
+    ConversationHandler,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    CallbackContext,
+)
+from threading import Lock
+from typing import List
 
 from pdf_bot.constants import CANCEL, BEAUTIFY, TO_PDF, REMOVE_LAST, TEXT_FILTER
 from pdf_bot.utils import (
@@ -20,17 +35,19 @@ WAIT_PHOTO = 0
 PHOTO_IDS = "photo_ids"
 PHOTO_NAMES = "photo_names"
 
+photo_locks = defaultdict(Lock)
 
-def photo_cov_handler():
+
+def photo_cov_handler() -> ConversationHandler:
+    handlers = [
+        MessageHandler(Filters.document | Filters.photo, check_photo, run_async=True),
+        MessageHandler(TEXT_FILTER, check_text, run_async=True),
+    ]
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler("photo", photo, run_async=True)],
         states={
-            WAIT_PHOTO: [
-                MessageHandler(
-                    Filters.document | Filters.photo, check_photo, run_async=True
-                ),
-                MessageHandler(TEXT_FILTER, check_text, run_async=True),
-            ]
+            WAIT_PHOTO: handlers,
+            ConversationHandler.WAITING: handlers,
         },
         fallbacks=[CommandHandler("cancel", cancel, run_async=True)],
         allow_reentry=True,
@@ -39,22 +56,27 @@ def photo_cov_handler():
     return conv_handler
 
 
-def photo(update, context):
+def photo(update: Update, context: CallbackContext) -> int:
+    update.effective_message.chat.send_action(ChatAction.TYPING)
+    user_id = update.effective_message.from_user.id
+    photo_locks[user_id].acquire()
     context.user_data[PHOTO_IDS] = []
     context.user_data[PHOTO_NAMES] = []
+    photo_locks[user_id].release()
 
     return ask_first_photo(update, context)
 
 
-def ask_first_photo(update, context):
+def ask_first_photo(update: Update, context: CallbackContext) -> int:
     _ = set_lang(update, context)
     reply_markup = ReplyKeyboardMarkup(
         [[_(CANCEL)]], resize_keyboard=True, one_time_keyboard=True
     )
     update.effective_message.reply_text(
         _(
-            "Send me the first photo that you'll like to beautify or convert into PDF\n\n"
-            "Note that the photos will be beautified and converted in the order that you send me"
+            "Send me the photos that you'll like to beautify or "
+            "convert into a PDF file\n\nNote that the photos will be beautified and "
+            "converted in the order that you send me"
         ),
         reply_markup=reply_markup,
     )
@@ -62,28 +84,35 @@ def ask_first_photo(update, context):
     return WAIT_PHOTO
 
 
-def check_photo(update, context):
-    _ = set_lang(update, context)
+def check_photo(update: Update, context: CallbackContext) -> int:
+    message = update.effective_message
+    message.chat.send_action(ChatAction.TYPING)
     photo_file = check_photo_file(update, context)
+    user_id = message.from_user.id
+    photo_locks[user_id].acquire()
 
     if photo_file is None:
         if not context.user_data[PHOTO_IDS]:
-            return ask_first_photo(update, context)
+            result = ask_first_photo(update, context)
         else:
-            return ask_next_photo(update, context)
+            result = ask_next_photo(update, context)
+    else:
+        _ = set_lang(update, context)
+        try:
+            file_name = photo_file.file_name
+        except AttributeError:
+            file_name = _("File name unavailable")
 
-    try:
-        file_name = photo_file.file_name
-    except AttributeError:
-        file_name = _("File name unavailable")
+        context.user_data[PHOTO_IDS].append(photo_file.file_id)
+        context.user_data[PHOTO_NAMES].append(file_name)
+        result = ask_next_photo(update, context)
 
-    context.user_data[PHOTO_IDS].append(photo_file.file_id)
-    context.user_data[PHOTO_NAMES].append(file_name)
+    photo_locks[user_id].release()
 
-    return ask_next_photo(update, context)
+    return result
 
 
-def check_photo_file(update, context):
+def check_photo_file(update: Update, context: CallbackContext):
     _ = set_lang(update, context)
     message = update.effective_message
 
@@ -102,8 +131,9 @@ def check_photo_file(update, context):
     return photo_file
 
 
-def ask_next_photo(update, context):
+def ask_next_photo(update: Update, context: CallbackContext) -> int:
     _ = set_lang(update, context)
+    send_file_names(update, context, context.user_data[PHOTO_NAMES], _("photos"))
     reply_markup = ReplyKeyboardMarkup(
         [[_(BEAUTIFY), _(TO_PDF)], [_(REMOVE_LAST), _(CANCEL)]],
         resize_keyboard=True,
@@ -111,32 +141,42 @@ def ask_next_photo(update, context):
     )
     update.effective_message.reply_text(
         _(
-            "Send me the next photo that you'll like to beautify or convert to PDF\n\n"
-            "Select the task from below if you have sent me all the photos"
+            "Select the task from below if you've sent me all the photos, or "
+            "send me the next photo that you'll like to beautify or convert\n\n"
         ),
         reply_markup=reply_markup,
     )
-    send_file_names(update, context, context.user_data[PHOTO_NAMES], _("photos"))
 
     return WAIT_PHOTO
 
 
-def check_text(update, context):
-    _ = set_lang(update, context)
+def check_text(update: Update, context: CallbackContext) -> int:
+    message = update.effective_message
+    message.chat.send_action(ChatAction.TYPING)
     text = update.effective_message.text
+    result = ConversationHandler.END
+    _ = set_lang(update, context)
 
-    if text == _(REMOVE_LAST):
-        return remove_photo(update, context)
-    if text in [_(BEAUTIFY), _(TO_PDF)]:
-        return process_all_photos(update, context)
+    if text in [_(REMOVE_LAST), _(BEAUTIFY), _(TO_PDF)]:
+        user_id = message.from_user.id
+        photo_locks[user_id].acquire()
+
+        if not check_user_data(update, context, PHOTO_IDS):
+            result = ConversationHandler.END
+        else:
+            if text == _(REMOVE_LAST):
+                result = remove_photo(update, context)
+            elif text in [_(BEAUTIFY), _(TO_PDF)]:
+                result = process_all_photos(update, context)
+
+        photo_locks[user_id].release()
     elif text == _(CANCEL):
-        return cancel(update, context)
+        result = cancel(update, context)
+
+    return result
 
 
-def remove_photo(update, context):
-    if not check_user_data(update, context, PHOTO_IDS):
-        return ConversationHandler.END
-
+def remove_photo(update: Update, context: CallbackContext) -> int:
     _ = set_lang(update, context)
     file_ids = context.user_data[PHOTO_IDS]
     file_names = context.user_data[PHOTO_NAMES]
@@ -154,10 +194,7 @@ def remove_photo(update, context):
         return ask_next_photo(update, context)
 
 
-def process_all_photos(update, context):
-    if not check_user_data(update, context, PHOTO_IDS):
-        return ConversationHandler.END
-
+def process_all_photos(update: Update, context: CallbackContext) -> int:
     user_data = context.user_data
     file_ids = user_data[PHOTO_IDS]
     file_names = user_data[PHOTO_NAMES]
@@ -176,7 +213,9 @@ def process_all_photos(update, context):
     return ConversationHandler.END
 
 
-def process_photo(update, context, file_ids, is_beautify):
+def process_photo(
+    update: Update, context: CallbackContext, file_ids: List[str], is_beautify: bool
+) -> None:
     _ = set_lang(update, context)
     if is_beautify:
         update.effective_message.reply_text(
