97d5067ab76287776d2f43fa34d5cf8633e45a5e
zeshuaro <joshuaystang@gmail.com>
2021-01-04 16:35:31 +1100

    Update split pdf instructions (#183)

--- pdf_bot/files/split.py
+++ pdf_bot/files/split.py
@@ -16,9 +16,24 @@ def ask_split_range(update, context):
     _ = set_lang(update, context)
     update.effective_message.reply_text(
         _(
-            "Send me the range of pages that you'll like to keep. "
-            "Use ⚡ <b>INSTANT VIEW</b> from below or refer to "
-            "[here](http://telegra.ph/Telegram-PDF-Bot-07-16) for some range examples."
+            "Send me the range of pages that you'll like to keep\n\n"
+            "<b>General usage</b>\n"
+            "<code>:      all pages</code>\n"
+            "<code>22     just the 23rd page</code>\n"
+            "<code>0:3    the first three pages</code>\n"
+            "<code>:3     the first three pages</code>\n"
+            "<code>5:     from the 6th page onwards</code>\n"
+            "<code>-1     last page only</code>\n"
+            "<code>:-1    all pages but the last page</code>\n"
+            "<code>-2     second last page only</code>\n"
+            "<code>-2:    last two pages</code>\n"
+            "<code>-3:-1  third and second last pages only</code>\n\n"
+            "<b>Advanced usage</b>\n"
+            "<code>::2    pages 0 2 4 ... to the end</code>\n"
+            "<code>1:10:2 pages 1 3 5 7 9</code>\n"
+            "<code>::-1   all pages in reversed order</code>\n"
+            "<code>3:0:-1 pages 3 2 1 but not 0</code>\n"
+            "<code>2::-1  pages 2 1 0</code>"
         ),
         parse_mode=ParseMode.HTML,
         reply_markup=get_back_markup(update, context),
