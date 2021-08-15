83f1976f5d3f22d56174ddb02dc529f28567a149
zeshuaro <joshuaystang@gmail.com>
2021-08-15 20:56:44 +1000

    chore: add logging for URL task (#421)

--- pdf_bot/url.py
+++ pdf_bot/url.py
@@ -3,6 +3,7 @@ import os
 import tempfile
 from urllib.parse import urlparse
 
+from logbook import Logger
 from telegram import Update
 from telegram.ext import CallbackContext
 from weasyprint import HTML
@@ -16,15 +17,18 @@ logging.getLogger("weasyprint").setLevel(100)
 
 
 def url_to_pdf(update: Update, context: CallbackContext):
+    log = Logger()
     _ = set_lang(update, context)
     message = update.effective_message
     url = message.text
     user_data = context.user_data
 
+    log.info(f"Received update {update} to convert URL to PDF")
     if user_data is not None and URLS in user_data and url in user_data[URLS]:
         message.reply_text(
             _("You've sent me this web page already and I'm still converting it")
         )
+        log.info(f"Finished update {update}, URL convert still in progress")
     else:
         message.reply_text(_("Converting your web page into a PDF file"))
         if URLS in user_data:
@@ -41,3 +45,4 @@ def url_to_pdf(update: Update, context: CallbackContext):
                 message.reply_text(_("Unable to reach your web page"))
 
         user_data[URLS].remove(url)
+        log.info(f"Finished update {update} to convert URL to PDF")
