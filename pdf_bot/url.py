709ab002820ea66ba1c90bb6f2a1cae645ae71c0
Joshua Tang <joshuaystang@gmail.com>
2022-02-21 19:00:42 +1100

    feat: add to handle timed out requests for URLs

--- pdf_bot/url.py
+++ pdf_bot/url.py
@@ -1,9 +1,9 @@
 import logging
 import os
 import tempfile
+import time
 from urllib.parse import urlparse
 
-from logbook import Logger
 from telegram import Update
 from telegram.ext import CallbackContext
 from weasyprint import HTML
@@ -17,24 +17,29 @@ logging.getLogger("weasyprint").setLevel(100)
 
 
 def url_to_pdf(update: Update, context: CallbackContext):
-    log = Logger()
     _ = set_lang(update, context)
     message = update.effective_message
     url = message.text
     user_data = context.user_data
 
-    log.info(f"Received update {update} to convert URL to PDF")
     if user_data is not None and URLS in user_data and url in user_data[URLS]:
-        message.reply_text(
-            _("You've sent me this web page already and I'm still converting it")
-        )
-        log.info(f"Finished update {update}, URL convert still in progress")
+        if time.time() - user_data[URLS][url] > 30:
+            message.reply_text(
+                _(
+                    "Timed out processing your web page, your web page is probably too "
+                    "complex to process"
+                )
+            )
+        else:
+            message.reply_text(
+                _("You've sent me this web page already and I'm still converting it")
+            )
     else:
         message.reply_text(_("Converting your web page into a PDF file"))
         if URLS in user_data:
-            user_data[URLS].add(url)
+            user_data[URLS][url] = time.time()
         else:
-            user_data[URLS] = {url}
+            user_data[URLS] = {url: time.time()}
 
         with tempfile.TemporaryDirectory() as dir_name:
             out_fn = os.path.join(dir_name, f"{urlparse(url).netloc}.pdf")
@@ -44,5 +49,7 @@ def url_to_pdf(update: Update, context: CallbackContext):
             except URLFetchingError:
                 message.reply_text(_("Unable to reach your web page"))
 
-        user_data[URLS].remove(url)
-        log.info(f"Finished update {update} to convert URL to PDF")
+    try:
+        del user_data[URLS][url]
+    except KeyError:
+        pass
