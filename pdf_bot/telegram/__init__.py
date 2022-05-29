ab3fedfc8c1a6dbd85b891e6322b66b999b3ed6d
Joshua Tang <joshuaystang@gmail.com>
2022-05-29 14:32:19 +1000

    feat: add check pdf document method to telegram service (#749)

--- pdf_bot/telegram/__init__.py
+++ pdf_bot/telegram/__init__.py
@@ -1,3 +1,13 @@
+from .exceptions import (
+    TelegramFileMimeTypeError,
+    TelegramFileTooLargeError,
+    TelegramServiceError,
+)
 from .telegram_service import TelegramService
 
-__all__ = ["TelegramService"]
+__all__ = [
+    "TelegramService",
+    "TelegramServiceError",
+    "TelegramFileMimeTypeError",
+    "TelegramFileTooLargeError",
+]
