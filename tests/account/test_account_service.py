471828e6686f4d9f217cea07c85aca862e8cd1ad
Joshua Tang <joshuaystang@gmail.com>
2022-10-27 00:13:32 +1100

    ci: setup mypy (#1015)

--- tests/account/test_account_service.py
+++ tests/account/test_account_service.py
@@ -7,27 +7,27 @@ from pdf_bot.account import AccountRepository, AccountService
 
 class TestAccountService:
     @classmethod
-    def setup_class(cls):
+    def setup_class(cls) -> None:
         cls.lang_code = "en_GB"
         cls.user_id = 0
 
-    def setup_method(self):
+    def setup_method(self) -> None:
         self.user = MagicMock(spec=User)
         self.user.id = self.user_id
 
         self.repo = MagicMock(spec=AccountRepository)
         self.service = AccountService(self.repo)
 
-    def test_create_user(self):
+    def test_create_user(self) -> None:
         self.service.create_user(self.user)
         self.repo.upsert_user.assert_called_with(self.user_id, self.lang_code)
 
-    def test_create_user_with_language_code(self):
+    def test_create_user_with_language_code(self) -> None:
         self.user.language_code = "it"
         self.service.create_user(self.user)
         self.repo.upsert_user.assert_called_with(self.user_id, "it_IT")
 
-    def test_create_user_with_invalid_language_code(self):
+    def test_create_user_with_invalid_language_code(self) -> None:
         self.user.language_code.return_value = "clearly_invalid_code"
         self.service.create_user(self.user)
         self.repo.upsert_user.assert_called_with(self.user_id, self.lang_code)
