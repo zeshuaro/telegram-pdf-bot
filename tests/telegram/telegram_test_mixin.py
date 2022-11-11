ccb2dda95489153ce4be40d4a8111edbdf98e0e2
Joshua Tang <joshuaystang@gmail.com>
2022-11-11 14:18:17 +1100

    test: refactor telegram service tests (#1036)

--- tests/telegram/telegram_test_mixin.py
+++ tests/telegram/telegram_test_mixin.py
@@ -1,6 +1,15 @@
 from unittest.mock import MagicMock
 
-from telegram import CallbackQuery, Document, Message, Update, User
+from telegram import (
+    Bot,
+    CallbackQuery,
+    Document,
+    File,
+    Message,
+    PhotoSize,
+    Update,
+    User,
+)
 from telegram.ext import CallbackContext
 
 
@@ -8,9 +17,11 @@ class TelegramTestMixin:
     @classmethod
     def setup_class(cls) -> None:
         cls.telegram_user_id = 0
+        cls.telegram_file_id = "file_id"
         cls.telegram_document_id = "document_id"
         cls.telegram_document_name = "document_name"
         cls.telegram_text = "text"
+        cls.telegram_chat_id = "chat_id"
 
     @classmethod
     def teardown_class(cls) -> None:
@@ -20,9 +31,14 @@ class TelegramTestMixin:
         self.telegram_user = MagicMock(spec=User)
         self.telegram_user.id = self.telegram_user_id
 
+        self.telegram_file = MagicMock(spec=File)
+        self.telegram_file.file_id = self.telegram_file_id
+
         self.telegram_document = MagicMock(spec=Document)
         self.telegram_document.file_id = self.telegram_document_id
         self.telegram_document.file_name = self.telegram_document_name
+
+        self.telegram_photo_size = MagicMock(spec=PhotoSize)
 
         self.telegram_message = MagicMock(spec=Message)
         self.telegram_message.from_user = self.telegram_user
@@ -36,6 +52,8 @@ class TelegramTestMixin:
 
         self.telegram_query = MagicMock(spec=CallbackQuery)
         self.telegram_query.from_user = self.telegram_user
+
+        self.telegram_bot = MagicMock(spec=Bot)
 
     def teardown_method(self) -> None:
         pass
--- tests/telegram/test_telegram_service.py
+++ tests/telegram/test_telegram_service.py
@@ -1,11 +1,9 @@
-from random import randint
-from typing import Callable, List
+from dataclasses import dataclass
 from unittest.mock import MagicMock, call
 
 import pytest
-from telegram import Bot, Document, File, Message, PhotoSize
+from telegram import File
 from telegram.constants import MAX_FILESIZE_DOWNLOAD
-from telegram.ext import CallbackContext
 
 from pdf_bot.io import IOService
 from pdf_bot.models import FileData
@@ -16,200 +14,175 @@ from pdf_bot.telegram import (
     TelegramService,
 )
 from pdf_bot.telegram.exceptions import TelegramUserDataKeyError
+from tests.telegram.telegram_test_mixin import TelegramTestMixin
 
 
-@pytest.fixture(name="telegram_service")
-def fixture_telegram_service(
-    io_service: IOService, telegram_bot: Bot
-) -> TelegramService:
-    return TelegramService(io_service, bot=telegram_bot)
+class TestTelegramRService(TelegramTestMixin):
+    @classmethod
+    def setup_class(cls) -> None:
+        super().setup_class()
+        cls.img_mime_type = "image"
+        cls.pdf_mime_type = "pdf"
+        cls.file_path = "file_path"
+        cls.user_data_key = "user_data_key"
+        cls.user_data_value = "user_data_value"
 
+    def setup_method(self) -> None:
+        super().setup_method()
+        self.io_service = MagicMock(spec=IOService)
+        self.sut = TelegramService(self.io_service, bot=self.telegram_bot)
 
-def test_check_file_size_too_large(
-    telegram_service: TelegramService,
-    telegram_document: Document,
-):
-    telegram_document.file_size = MAX_FILESIZE_DOWNLOAD + 1
-    with pytest.raises(TelegramFileTooLargeError):
-        telegram_service.check_file_size(telegram_document)
+    def test_check_file_size(self) -> None:
+        self.telegram_document.file_size = MAX_FILESIZE_DOWNLOAD
+        self.sut.check_file_size(self.telegram_document)
 
+    def test_check_file_size_too_large(self) -> None:
+        self.telegram_document.file_size = MAX_FILESIZE_DOWNLOAD + 1
+        with pytest.raises(TelegramFileTooLargeError):
+            self.sut.check_file_size(self.telegram_document)
 
-def test_check_image_document(
-    telegram_service: TelegramService,
-    telegram_message: Message,
-    telegram_document: Document,
-):
-    telegram_document.mime_type = "image"
-    telegram_document.file_size = MAX_FILESIZE_DOWNLOAD
-    telegram_message.document = telegram_document
+    def test_check_image_document(self) -> None:
+        self.telegram_document.mime_type = self.img_mime_type
+        self.telegram_document.file_size = MAX_FILESIZE_DOWNLOAD
+        self.telegram_message.document = self.telegram_document
 
-    actual = telegram_service.check_image(telegram_message)
+        actual = self.sut.check_image(self.telegram_message)
 
-    assert actual == telegram_document
+        assert actual == self.telegram_document
 
+    def test_check_image_document_invalid_mime_type(self) -> None:
+        self.telegram_document.mime_type = "clearly_invalid"
+        self.telegram_message.document = self.telegram_document
 
-def test_check_image_document_invalid_mime_type(
-    telegram_service: TelegramService,
-    telegram_message: Message,
-    telegram_document: Document,
-):
-    telegram_document.mime_type = "clearly_random"
-    telegram_message.document = telegram_document
+        with pytest.raises(TelegramFileMimeTypeError):
+            self.sut.check_image(self.telegram_message)
 
-    with pytest.raises(TelegramFileMimeTypeError):
-        telegram_service.check_image(telegram_message)
+    def test_check_image_document_too_large(self) -> None:
+        self.telegram_document.mime_type = self.img_mime_type
+        self.telegram_document.file_size = MAX_FILESIZE_DOWNLOAD + 1
+        self.telegram_message.document = self.telegram_document
 
+        with pytest.raises(TelegramFileTooLargeError):
+            self.sut.check_image(self.telegram_message)
 
-def test_check_image_document_too_large(
-    telegram_service: TelegramService,
-    telegram_message: Message,
-    telegram_document: Document,
-):
-    telegram_document.mime_type = "image"
-    telegram_document.file_size = MAX_FILESIZE_DOWNLOAD + 1
-    telegram_message.document = telegram_document
+    def test_check_image(self) -> None:
+        self.telegram_photo_size.file_size = MAX_FILESIZE_DOWNLOAD
+        self.telegram_message.document = None
+        self.telegram_message.photo = [self.telegram_photo_size]
 
-    with pytest.raises(TelegramFileTooLargeError):
-        telegram_service.check_image(telegram_message)
+        actual = self.sut.check_image(self.telegram_message)
 
+        assert actual == self.telegram_photo_size
 
-def test_check_image(
-    telegram_service: TelegramService,
-    telegram_message: Message,
-    telegram_photo_size: PhotoSize,
-):
-    telegram_photo_size.file_size = MAX_FILESIZE_DOWNLOAD
-    telegram_message.document = None
-    telegram_message.photo = [telegram_photo_size]
+    def test_check_image_not_found(self) -> None:
+        self.telegram_message.document = None
+        self.telegram_message.photo = []
 
-    actual = telegram_service.check_image(telegram_message)
+        with pytest.raises(TelegramImageNotFoundError):
+            self.sut.check_image(self.telegram_message)
 
-    assert actual == telegram_photo_size
+    def test_check_image_too_large(self) -> None:
+        self.telegram_photo_size.file_size = MAX_FILESIZE_DOWNLOAD + 1
+        self.telegram_message.document = None
+        self.telegram_message.photo = [self.telegram_photo_size]
 
+        with pytest.raises(TelegramFileTooLargeError):
+            self.sut.check_image(self.telegram_message)
 
-def test_check_image_too_large(
-    telegram_service: TelegramService,
-    telegram_message: Message,
-    telegram_photo_size: PhotoSize,
-):
-    telegram_photo_size.file_size = MAX_FILESIZE_DOWNLOAD + 1
-    telegram_message.document = None
-    telegram_message.photo = [telegram_photo_size]
+    def test_check_pdf_document(self) -> None:
+        self.telegram_document.mime_type = self.pdf_mime_type
+        self.telegram_document.file_size = MAX_FILESIZE_DOWNLOAD
+        self.telegram_message.document = self.telegram_document
 
-    with pytest.raises(TelegramFileTooLargeError):
-        telegram_service.check_image(telegram_message)
+        actual = self.sut.check_pdf_document(self.telegram_message)
 
+        assert actual == self.telegram_document
 
-def test_check_image_not_found(
-    telegram_service: TelegramService,
-    telegram_message: Message,
-    telegram_photo_size: PhotoSize,
-):
-    telegram_photo_size.file_size = MAX_FILESIZE_DOWNLOAD + 1
-    telegram_message.document = None
-    telegram_message.photo = []
+    def test_check_pdf_document_invalid_mime_type(self) -> None:
+        self.telegram_document.mime_type = "clearly_invalid"
+        self.telegram_message.document = self.telegram_document
 
-    with pytest.raises(TelegramImageNotFoundError):
-        telegram_service.check_image(telegram_message)
+        with pytest.raises(TelegramFileMimeTypeError):
+            self.sut.check_pdf_document(self.telegram_message)
 
+    def test_check_pdf_document_too_large(self) -> None:
+        self.telegram_document.mime_type = self.pdf_mime_type
+        self.telegram_document.file_size = MAX_FILESIZE_DOWNLOAD + 1
+        self.telegram_message.document = self.telegram_document
 
-def test_check_pdf_document(
-    telegram_service: TelegramService,
-    telegram_message: Message,
-    telegram_document: Document,
-):
-    telegram_document.mime_type = "pdf"
-    telegram_document.file_size = MAX_FILESIZE_DOWNLOAD
-    telegram_message.document = telegram_document
+        with pytest.raises(TelegramFileTooLargeError):
+            self.sut.check_pdf_document(self.telegram_message)
 
-    actual = telegram_service.check_pdf_document(telegram_message)
+    def test_download_file(self) -> None:
+        self.io_service.create_temp_file.return_value.__enter__.return_value = (
+            self.file_path
+        )
+        self.telegram_bot.get_file.return_value = self.telegram_file
 
-    assert actual == telegram_document
+        with self.sut.download_file(self.telegram_file_id) as actual:
+            assert actual == self.file_path
+            self.telegram_bot.get_file.assert_called_with(self.telegram_file_id)
+            self.telegram_file.download.assert_called_once_with(
+                custom_path=self.file_path
+            )
 
+    @pytest.mark.parametrize("num_files", [1, 2, 5])
+    def test_download_files(self, num_files: int) -> None:
+        @dataclass
+        class FileAndPath:
+            file: File
+            path: str
 
-def test_check_pdf_document_invalid_mime_type(
-    telegram_service: TelegramService,
-    telegram_message: Message,
-    telegram_document: Document,
-):
-    telegram_document.mime_type = "clearly_random"
-    telegram_message.document = telegram_document
+        file_ids: list[str] = []
+        file_paths: list[str] = []
+        files: dict[str, FileAndPath] = {}
 
-    with pytest.raises(TelegramFileMimeTypeError):
-        telegram_service.check_pdf_document(telegram_message)
+        for i in range(num_files):
+            file_id = f"file_id_{i}"
+            file_path = f"file_path_{i}"
+            file_ids.append(file_id)
+            file_paths.append(file_path)
 
+            file = MagicMock(spec=File)
+            files[file_id] = FileAndPath(file, file_path)
 
-def test_check_pdf_document_too_large(
-    telegram_service: TelegramService,
-    telegram_message: Message,
-    telegram_document: Document,
-):
-    telegram_document.mime_type = "pdf"
-    telegram_document.file_size = MAX_FILESIZE_DOWNLOAD + 1
-    telegram_message.document = telegram_document
+        self.io_service.create_temp_files.return_value.__enter__.return_value = (
+            file_paths
+        )
+        self.telegram_bot.get_file.side_effect = lambda file_id: files[file_id].file
 
-    with pytest.raises(TelegramFileTooLargeError):
-        telegram_service.check_pdf_document(telegram_message)
+        with self.sut.download_files(file_ids) as actual:
+            assert actual == file_paths
 
+            get_file_calls = [call(file_id) for file_id in file_ids]
+            self.telegram_bot.get_file.assert_has_calls(get_file_calls)
 
-def test_download_file(
-    telegram_service: TelegramService,
-    telegram_bot: Bot,
-    telegram_file: File,
-    document_id: str,
-):
-    telegram_bot.get_file.return_value = telegram_file
-    with telegram_service.download_file(document_id):
-        telegram_bot.get_file.assert_called_with(document_id)
-        telegram_file.download.assert_called()
+            for file_and_path in files.values():
+                file_and_path.file.download.assert_called_once_with(
+                    custom_path=file_and_path.path
+                )
 
+    def test_get_user_data(self) -> None:
+        self.telegram_context.user_data = {self.user_data_key: self.user_data_value}
 
-def test_download_files(
-    telegram_service: TelegramService,
-    telegram_bot: Bot,
-    document_ids_generator: Callable[[int], List[str]],
-):
-    def get_file_side_effect(doc_id: str):
-        mock = MagicMock()
-        mock.__enter__.return_value = File(doc_id, doc_id)
-        return mock
+        actual = self.sut.get_user_data(self.telegram_context, self.user_data_key)
 
-    num_files = randint(2, 10)
-    file_ids = document_ids_generator(num_files)
-    telegram_bot.get_file.side_effect = get_file_side_effect
+        assert actual == self.user_data_value
+        assert self.user_data_key not in self.telegram_context.user_data
 
-    with telegram_service.download_files(file_ids):
-        calls = [call(file_id) for file_id in file_ids]
-        telegram_bot.get_file.assert_has_calls(calls)
+    def test_get_user_data_key_error(self) -> None:
+        self.telegram_context.user_data = {}
 
+        with pytest.raises(TelegramUserDataKeyError):
+            self.sut.get_user_data(self.telegram_context, self.user_data_key)
 
-def test_get_user_data(
-    telegram_service: TelegramService, telegram_context: CallbackContext
-):
-    key = "key"
-    value = "value"
-    telegram_context.user_data = {key: value}
+    def test_send_file_names(self) -> None:
+        file_data_list = [FileData("a", "a"), FileData("b", "b")]
 
-    actual = telegram_service.get_user_data(telegram_context, key)
+        self.sut.send_file_names(
+            self.telegram_chat_id, self.telegram_text, file_data_list
+        )
 
-    assert actual == value
-    assert key not in telegram_context.user_data
-
-
-def test_get_user_data_key_error(
-    telegram_service: TelegramService, telegram_context: CallbackContext
-):
-    telegram_context.user_data = {}
-
-    with pytest.raises(TelegramUserDataKeyError):
-        telegram_service.get_user_data(telegram_context, "key")
-
-
-def test_send_file_names(telegram_service: TelegramService, telegram_bot: Bot):
-    chat_id = "chat_id"
-    text = "text"
-    file_data_list = [FileData("a", "a"), FileData("b", "b")]
-
-    telegram_service.send_file_names(chat_id, text, file_data_list)
-
-    telegram_bot.send_message.assert_called_once_with(chat_id, f"{text}1: a\n2: b\n")
+        self.telegram_bot.send_message.assert_called_once_with(
+            self.telegram_chat_id, f"{self.telegram_text}1: a\n2: b\n"
+        )
