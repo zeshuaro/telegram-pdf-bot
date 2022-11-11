4eeb52223750575afcfb5817ee0d34ef2fb3dc63
Joshua Tang <joshuaystang@gmail.com>
2022-11-11 15:40:45 +1100

    refactor: update compare service and tests (#1043)
    * refactor: update compare service and tests
    
    * test: imporve compare service test coverage
    
    * fix: app containers setup

--- pdf_bot/compare/compare_service.py
+++ pdf_bot/compare/compare_service.py
@@ -3,28 +3,33 @@ from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
 from telegram.ext import CallbackContext, ConversationHandler
 
 from pdf_bot.analytics import TaskType
-from pdf_bot.compare.constants import COMPARE_ID, WAIT_FIRST_PDF, WAIT_SECOND_PDF
 from pdf_bot.consts import BACK, CANCEL
-from pdf_bot.language import set_lang
+from pdf_bot.language_new import LanguageService
 from pdf_bot.pdf import PdfService
 from pdf_bot.telegram import (
     TelegramService,
     TelegramServiceError,
     TelegramUserDataKeyError,
 )
-from pdf_bot.utils import cancel, send_result_file
 
 
 class CompareService:
+    WAIT_FIRST_PDF = 0
+    WAIT_SECOND_PDF = 1
+    _COMPARE_ID = "compare_id"
+
     def __init__(
-        self, pdf_service: PdfService, telegram_service: TelegramService
+        self,
+        pdf_service: PdfService,
+        telegram_service: TelegramService,
+        language_service: LanguageService,
     ) -> None:
         self.pdf_service = pdf_service
         self.telegram_service = telegram_service
+        self.language_service = language_service
 
-    @staticmethod
-    def ask_first_pdf(update: Update, context: CallbackContext) -> int:
-        _ = set_lang(update, context)
+    def ask_first_pdf(self, update: Update, context: CallbackContext) -> int:
+        _ = self.language_service.set_app_language(update, context)
         reply_markup = ReplyKeyboardMarkup(
             [[_(CANCEL)]], resize_keyboard=True, one_time_keyboard=True
         )
@@ -36,19 +41,19 @@ class CompareService:
             reply_markup=reply_markup,
         )
 
-        return WAIT_FIRST_PDF
+        return self.WAIT_FIRST_PDF
 
     def check_first_pdf(self, update: Update, context: CallbackContext) -> int:
-        _ = set_lang(update, context)
+        _ = self.language_service.set_app_language(update, context)
         message = update.effective_message
 
         try:
             doc = self.telegram_service.check_pdf_document(message)
         except TelegramServiceError as e:
             message.reply_text(_(str(e)))
-            return WAIT_FIRST_PDF
+            return self.WAIT_FIRST_PDF
 
-        context.user_data[COMPARE_ID] = doc.file_id
+        context.user_data[self._COMPARE_ID] = doc.file_id
         reply_markup = ReplyKeyboardMarkup(
             [[_(BACK), _(CANCEL)]], resize_keyboard=True, one_time_keyboard=True
         )
@@ -57,20 +62,20 @@ class CompareService:
             reply_markup=reply_markup,
         )
 
-        return WAIT_SECOND_PDF
+        return self.WAIT_SECOND_PDF
 
     def compare_pdfs(self, update: Update, context: CallbackContext) -> int:
-        _ = set_lang(update, context)
+        _ = self.language_service.set_app_language(update, context)
         message = update.effective_message
 
         try:
             doc = self.telegram_service.check_pdf_document(message)
-            file_id = self.telegram_service.get_user_data(context, COMPARE_ID)
+            file_id = self.telegram_service.get_user_data(context, self._COMPARE_ID)
         except TelegramServiceError as e:
             message.reply_text(_(str(e)))
             if isinstance(e, TelegramUserDataKeyError):
                 return ConversationHandler.END
-            return WAIT_SECOND_PDF
+            return self.WAIT_SECOND_PDF
 
         message.reply_text(
             _("Comparing your PDF files"), reply_markup=ReplyKeyboardRemove()
@@ -78,7 +83,9 @@ class CompareService:
 
         try:
             with self.pdf_service.compare_pdfs(file_id, doc.file_id) as out_path:
-                send_result_file(update, context, out_path, TaskType.compare_pdf)
+                self.telegram_service.reply_with_file(
+                    update, context, out_path, TaskType.compare_pdf
+                )
         except NoDifferenceError:
             message.reply_text(
                 _("There are no text differences between your PDF files")
@@ -87,12 +94,12 @@ class CompareService:
         return ConversationHandler.END
 
     def check_text(self, update: Update, context: CallbackContext) -> int | None:
-        _ = set_lang(update, context)
+        _ = self.language_service.set_app_language(update, context)
         text = update.effective_message.text
 
         if text == _(BACK):
             return self.ask_first_pdf(update, context)
         if text == _(CANCEL):
-            return cancel(update, context)
+            return self.telegram_service.cancel_conversation(update, context)
 
         return None
--- pdf_bot/compare/constants.py
+++
@@ -1,3 +0,0 @@
-WAIT_FIRST_PDF = 0
-WAIT_SECOND_PDF = 1
-COMPARE_ID = "compare_id"
