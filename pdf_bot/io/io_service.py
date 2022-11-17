136ef85ec52e555688d85c7dbb7d630ad5058aff
Joshua Tang <joshuaystang@gmail.com>
2022-11-18 00:03:38 +1100

    refactor: update io service and tests

--- pdf_bot/io/io_service.py
+++ pdf_bot/io/io_service.py
@@ -41,24 +41,15 @@ class IOService:
     def create_temp_pdf_file(
         self, prefix: str | None = None
     ) -> Generator[str, None, None]:
-        try:
-            with self.create_temp_file(prefix=prefix, suffix=".pdf") as out_path:
-                yield out_path
-        finally:
-            pass
+        with self.create_temp_file(prefix=prefix, suffix=".pdf") as out_path:
+            yield out_path
 
     @contextmanager
     def create_temp_png_file(self, prefix: str) -> Generator[str, None, None]:
-        try:
-            with self.create_temp_file(prefix=prefix, suffix=".png") as out_path:
-                yield out_path
-        finally:
-            pass
+        with self.create_temp_file(prefix=prefix, suffix=".png") as out_path:
+            yield out_path
 
     @contextmanager
     def create_temp_txt_file(self, prefix: str) -> Generator[str, None, None]:
-        try:
-            with self.create_temp_file(prefix=prefix, suffix=".txt") as out_path:
-                yield out_path
-        finally:
-            pass
+        with self.create_temp_file(prefix=prefix, suffix=".txt") as out_path:
+            yield out_path
--- tests/io/test_io_service.py
+++ tests/io/test_io_service.py
@@ -1,57 +1,113 @@
-import os
-from random import randint
-from unittest.mock import patch
+from tempfile import TemporaryDirectory
+from unittest.mock import MagicMock, patch
 
 import pytest
 
 from pdf_bot.io import IOService
 
 
-@pytest.fixture(name="io_service")
-def fixture_io_service() -> IOService:
-    return IOService()
+class TestIOService:
+    DIR_NAME = "dir_name"
+    FILE_NAME = "file_name"
+    FILE_PREFIX = "file_prefix"
+    FILE_PREFIX_UNDERSCORE = f"{FILE_PREFIX}_"
+    FILE_SUFFIX = "file_suffix"
 
+    def setup_method(self) -> None:
+        self.tf = MagicMock()
+        self.tf.name = self.FILE_NAME
 
-def test_create_temp_directory(io_service: IOService):
-    with patch(
-        "pdf_bot.io.io_service.TemporaryDirectory"
-    ) as temporary_directory, io_service.create_temp_directory():
-        temporary_directory.assert_called_once()
+        self.tf_cls_patcher = patch(
+            "pdf_bot.io.io_service.NamedTemporaryFile", return_value=self.tf
+        )
+        self.tf_cls = self.tf_cls_patcher.start()
 
+        self.sut = IOService()
 
-def test_create_temp_file_with_prefix(io_service: IOService):
-    prefix = "prefix"
-    with io_service.create_temp_file(prefix=prefix) as out_path:
-        paths = os.path.split(out_path)
-        assert paths[1].startswith(prefix)
+    def teardown_method(self) -> None:
+        self.tf_cls_patcher.stop()
 
+    def test_create_temp_directory(self) -> None:
+        td = MagicMock(spec=TemporaryDirectory)
+        with patch("pdf_bot.io.io_service.TemporaryDirectory") as td_cls:
+            td_cls.return_value = td
+            td.name = self.DIR_NAME
 
-def test_create_temp_file_with_suffix(io_service: IOService):
-    suffix = "suffix"
-    with io_service.create_temp_file(suffix=suffix) as out_path:
-        paths = os.path.split(out_path)
-        assert paths[1].endswith(suffix)
+            with self.sut.create_temp_directory() as actual:
+                assert actual == self.DIR_NAME
 
+            td_cls.assert_called_once()
+            td.cleanup.assert_called_once()
 
-def test_create_temp_files(io_service: IOService):
-    num_files = randint(2, 10)
-    with io_service.create_temp_files(num_files) as out_paths:
-        assert len(out_paths) == num_files
+    @pytest.mark.parametrize(
+        "prefix,suffix",
+        [
+            (None, None),
+            (FILE_PREFIX, None),
+            (FILE_PREFIX_UNDERSCORE, None),
+            (None, FILE_SUFFIX),
+            (FILE_PREFIX, FILE_SUFFIX),
+            (FILE_PREFIX_UNDERSCORE, FILE_SUFFIX),
+        ],
+    )
+    def test_create_temp_file(self, prefix: str | None, suffix: str | None) -> None:
+        with self.sut.create_temp_file(prefix, suffix) as actual:
+            assert actual == self.FILE_NAME
 
+        expected_prefix = prefix
+        if prefix is not None and not prefix.endswith("_"):
+            expected_prefix += "_"
+        self._assert_temp_file(expected_prefix, suffix)
 
-def test_create_temp_pdf_file(io_service: IOService):
-    with io_service.create_temp_pdf_file("prefix") as out_path:
-        paths = os.path.split(out_path)
-        assert paths[1].endswith(".pdf")
+    @pytest.mark.parametrize("num_files", [0, 1, 2, 5])
+    def test_create_temp_files(self, num_files: int) -> None:
+        files = []
+        names = []
 
+        for i in range(num_files):
+            name = f"{self.FILE_NAME}_{i}"
+            file = MagicMock()
+            file.name = name
 
-def test_create_temp_png_file(io_service: IOService):
-    with io_service.create_temp_png_file("prefix") as out_path:
-        paths = os.path.split(out_path)
-        assert paths[1].endswith(".png")
+            files.append(file)
+            names.append(name)
 
+        index = 0
 
-def test_create_temp_txt_file(io_service: IOService):
-    with io_service.create_temp_txt_file("prefix") as out_path:
-        paths = os.path.split(out_path)
-        assert paths[1].endswith(".txt")
+        def create_tmp_file() -> MagicMock:
+            nonlocal index
+            file = files[index]
+            index += 1
+            return file
+
+        with patch("pdf_bot.io.io_service.NamedTemporaryFile") as tf:
+            tf.side_effect = create_tmp_file
+            with self.sut.create_temp_files(num_files) as actual:
+                assert actual == names
+
+            for file in files:
+                file.close.assert_called_once()
+
+    @pytest.mark.parametrize("prefix", [None, FILE_PREFIX, FILE_PREFIX_UNDERSCORE])
+    def test_create_temp_pdf_file(self, prefix: str | None) -> None:
+        with self.sut.create_temp_pdf_file(prefix) as actual:
+            assert actual == self.FILE_NAME
+
+        expected_prefix = prefix
+        if prefix is not None and not prefix.endswith("_"):
+            expected_prefix += "_"
+        self._assert_temp_file(expected_prefix, ".pdf")
+
+    def test_create_temp_png_file(self) -> None:
+        with self.sut.create_temp_png_file(self.FILE_PREFIX_UNDERSCORE) as actual:
+            assert actual == self.FILE_NAME
+        self._assert_temp_file(self.FILE_PREFIX_UNDERSCORE, ".png")
+
+    def test_create_temp_txt_file(self) -> None:
+        with self.sut.create_temp_txt_file(self.FILE_PREFIX_UNDERSCORE) as actual:
+            assert actual == self.FILE_NAME
+        self._assert_temp_file(self.FILE_PREFIX_UNDERSCORE, ".txt")
+
+    def _assert_temp_file(self, prefix: str | None, suffix: str | None) -> None:
+        self.tf_cls.assert_called_once_with(prefix=prefix, suffix=suffix)
+        self.tf.close.assert_called_once()
