0584106fbf190a0ac49c6078d0055c02a61eb206
Joshua Tang <joshuaystang@gmail.com>
2022-11-19 18:11:40 +1100

    refactor: update scale service and tests

--- pdf_bot/scale/scale_service.py
+++ pdf_bot/scale/scale_service.py
@@ -1,33 +1,45 @@
+from contextlib import contextmanager
+from gettext import gettext as _
+from typing import Generator
+
 from telegram import ParseMode, ReplyKeyboardMarkup, Update
 from telegram.ext import CallbackContext, ConversationHandler
 
 from pdf_bot.analytics import TaskType
-from pdf_bot.consts import BACK, PDF_INFO
-from pdf_bot.file_task import FileTaskService
-from pdf_bot.language import set_lang
-from pdf_bot.pdf import PdfService, ScaleData
+from pdf_bot.consts import BACK
+from pdf_bot.file_processor import AbstractFileProcessor
+from pdf_bot.pdf import ScaleData
 from pdf_bot.pdf.models import ScaleByData, ScaleToData
-from pdf_bot.scale import scale_constants
-from pdf_bot.telegram_internal import TelegramService, TelegramServiceError
-from pdf_bot.utils import send_result_file
 
 
-class ScaleService:
-    def __init__(
-        self,
-        file_task_service: FileTaskService,
-        pdf_service: PdfService,
-        telegram_service: TelegramService,
-    ) -> None:
-        self.file_task_service = file_task_service
-        self.pdf_service = pdf_service
-        self.telegram_service = telegram_service
+class ScaleService(AbstractFileProcessor):
+    BY_SCALING_FACTOR = _("By scaling factor")
+    TO_DIMENSION = _("To dimension")
 
-    @staticmethod
-    def ask_scale_type(update: Update, context: CallbackContext):
-        _ = set_lang(update, context)
+    WAIT_SCALE_TYPE = "wait_scale_type"
+    WAIT_SCALE_FACTOR = "wait_scale_factor"
+    WAIT_SCALE_DIMENSION = "wait_scale_dimension"
+
+    @property
+    def task_type(self) -> TaskType:
+        return TaskType.scale_pdf
+
+    @property
+    def should_process_back_option(self) -> bool:
+        return False
+
+    @contextmanager
+    def process_file_task(
+        self, file_id: str, message_text: str
+    ) -> Generator[str, None, None]:
+        scale_data = ScaleData.from_string(message_text)
+        with self.pdf_service.scale_pdf(file_id, scale_data) as path:
+            yield path
+
+    def ask_scale_type(self, update: Update, context: CallbackContext) -> str:
+        _ = self.language_service.set_app_language(update, context)
         keyboard = [
-            [_(scale_constants.BY_SCALING_FACTOR), _(scale_constants.TO_DIMENSION)],
+            [_(self.BY_SCALING_FACTOR), _(self.TO_DIMENSION)],
             [_(BACK)],
         ]
         reply_markup = ReplyKeyboardMarkup(
@@ -38,23 +50,25 @@ class ScaleService:
             reply_markup=reply_markup,
         )
 
-        return scale_constants.WAIT_SCALE_TYPE
+        return self.WAIT_SCALE_TYPE
 
-    def check_scale_type(self, update, context):
-        _ = set_lang(update, context)
+    def check_scale_type(self, update: Update, context: CallbackContext) -> str:
+        _ = self.language_service.set_app_language(update, context)
         text = update.effective_message.text
 
         if text in {
-            _(scale_constants.BY_SCALING_FACTOR),
-            _(scale_constants.TO_DIMENSION),
+            _(self.BY_SCALING_FACTOR),
+            _(self.TO_DIMENSION),
         }:
             return self._ask_scale_value(update, context)
         if text == _(BACK):
             return self.file_task_service.ask_pdf_task(update, context)
-        return scale_constants.WAIT_SCALE_TYPE
+        return self.WAIT_SCALE_TYPE
 
-    def scale_pdf_by_factor(self, update: Update, context: CallbackContext):
-        _ = set_lang(update, context)
+    def scale_pdf_by_factor(
+        self, update: Update, context: CallbackContext
+    ) -> str | int:
+        _ = self.language_service.set_app_language(update, context)
         message = update.effective_message
         text = message.text
 
@@ -62,7 +76,7 @@ class ScaleService:
             return self.ask_scale_type(update, context)
 
         try:
-            scale_data = ScaleByData.from_string(text)
+            ScaleByData.from_string(text)
         except ValueError:
             message.reply_text(
                 _("The scaling factors {values} are invalid, please try again").format(
@@ -70,11 +84,15 @@ class ScaleService:
                 ),
                 parse_mode=ParseMode.HTML,
             )
-            return scale_constants.WAIT_SCALE_FACTOR
-        return self._scale_pdf(update, context, scale_data)
+            return self.WAIT_SCALE_FACTOR
 
-    def scale_pdf_to_dimension(self, update: Update, context: CallbackContext):
-        _ = set_lang(update, context)
+        self.process_file(update, context)
+        return ConversationHandler.END
+
+    def scale_pdf_to_dimension(
+        self, update: Update, context: CallbackContext
+    ) -> str | int:
+        _ = self.language_service.set_app_language(update, context)
         message = update.effective_message
         text = message.text
 
@@ -82,7 +100,7 @@ class ScaleService:
             return self.ask_scale_type(update, context)
 
         try:
-            scale_data = ScaleToData.from_string(text)
+            ScaleToData.from_string(text)
         except ValueError:
             message.reply_text(
                 _("The dimensions {values} are invalid, please try again").format(
@@ -90,19 +108,17 @@ class ScaleService:
                 ),
                 parse_mode=ParseMode.HTML,
             )
-            return scale_constants.WAIT_SCALE_DIMENSION
-        return self._scale_pdf(update, context, scale_data)
+            return self.WAIT_SCALE_DIMENSION
 
-    @staticmethod
-    def _ask_scale_value(update: Update, context: CallbackContext):
-        _ = set_lang(update, context)
-        message = update.effective_message
-        reply_markup = ReplyKeyboardMarkup(
-            [[_(BACK)]], one_time_keyboard=True, resize_keyboard=True
-        )
+        self.process_file(update, context)
+        return ConversationHandler.END
 
-        if message.text == _(scale_constants.BY_SCALING_FACTOR):
-            message.reply_text(
+    def _ask_scale_value(self, update: Update, context: CallbackContext) -> str:
+        _ = self.language_service.set_app_language(update, context)
+        if update.effective_message.text == _(self.BY_SCALING_FACTOR):
+            self.telegram_service.reply_with_back_markup(
+                update,
+                context,
                 "{desc_1}\n{desc_2}\n\n{desc_3}".format(
                     desc_1=_(
                         "Send me the scaling factors for the horizontal "
@@ -114,34 +130,18 @@ class ScaleService:
                         "and halve the vertical axis"
                     ),
                 ),
-                reply_markup=reply_markup,
                 parse_mode=ParseMode.HTML,
             )
-            return scale_constants.WAIT_SCALE_FACTOR
+            return self.WAIT_SCALE_FACTOR
 
-        message.reply_text(
+        self.telegram_service.reply_with_back_markup(
+            update,
+            context,
             "{desc_1}\n{desc_2}\n\n{desc_3}".format(
                 desc_1=_("Send me the width and height"),
                 desc_2=f"<b>{_('Example: 150 200')}</b>",
                 desc_3=_("This will set the width to 150 and height to 200"),
             ),
-            reply_markup=reply_markup,
             parse_mode=ParseMode.HTML,
         )
-        return scale_constants.WAIT_SCALE_DIMENSION
-
-    def _scale_pdf(
-        self, update: Update, context: CallbackContext, scale_data: ScaleData
-    ):
-        _ = set_lang(update, context)
-        message = update.effective_message
-
-        try:
-            file_id, _file_name = self.telegram_service.get_user_data(context, PDF_INFO)
-        except TelegramServiceError as e:
-            message.reply_text(_(str(e)))
-            return ConversationHandler.END
-
-        with self.pdf_service.scale_pdf(file_id, scale_data) as out_path:
-            send_result_file(update, context, out_path, TaskType.scale_pdf)
-        return ConversationHandler.END
+        return self.WAIT_SCALE_DIMENSION
