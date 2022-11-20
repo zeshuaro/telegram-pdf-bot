26b3362563c35af5c3bb8d6e13318ed3ef61b017
Joshua Tang <joshuaystang@gmail.com>
2022-11-20 17:33:10 +1100

    chore: cleanup merge consts

--- pdf_bot/merge/merge_handlers.py
+++ pdf_bot/merge/merge_handlers.py
@@ -1,7 +1,6 @@
 from telegram.ext import CommandHandler, ConversationHandler, Filters, MessageHandler
 
 from pdf_bot.consts import TEXT_FILTER
-from pdf_bot.merge.constants import WAIT_MERGE_PDF
 from pdf_bot.merge.merge_service import MergeService
 from pdf_bot.utils import cancel
 
@@ -14,7 +13,7 @@ class MergeHandlers:
         return ConversationHandler(
             entry_points=[CommandHandler("merge", self.merge_service.ask_first_pdf)],
             states={
-                WAIT_MERGE_PDF: [
+                MergeService.WAIT_MERGE_PDF: [
                     MessageHandler(Filters.document, self.merge_service.check_pdf),
                     MessageHandler(TEXT_FILTER, self.merge_service.check_text),
                 ],
