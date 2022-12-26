2f545398adc6203587ad502522f52429204f2ee4
Joshua Tang <joshuaystang@gmail.com>
2022-12-27 00:13:52 +1100

    chore: increase request read and connect timeout to 30s (#1207)

--- pdf_bot/settings.py
+++ pdf_bot/settings.py
@@ -13,9 +13,9 @@ class Settings(BaseSettings):
     port: int = Field(default=8443, env="port")
 
     request_connection_pool_size: int = 8
-    request_read_timeout: int = 10
+    request_read_timeout: int = 30
     request_write_timeout: int = 30
-    request_connect_timeout: int = 10
+    request_connect_timeout: int = 30
     request_pool_timeout: int = 30
 
     sentry_dsn: str | None = Field(env="sentry_dsn")
