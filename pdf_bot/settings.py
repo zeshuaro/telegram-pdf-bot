a807e1f52d80ffbce8261cb2f1d28c6e2d9d08b9
Joshua Tang <joshuaystang@gmail.com>
2022-12-26 16:13:02 +1100

    feat: add to configure request timeouts (#1195)

--- pdf_bot/settings.py
+++ pdf_bot/settings.py
@@ -12,4 +12,10 @@ class Settings(BaseSettings):
     app_url: str | None = Field(default=None, env="app_url")
     port: int = Field(default=8443, env="port")
 
+    request_connection_pool_size: int = 8
+    request_read_timeout: int = 10
+    request_write_timeout: int = 30
+    request_connect_timeout: int = 10
+    request_pool_timeout: int = 30
+
     sentry_dsn: str | None = Field(env="sentry_dsn")
