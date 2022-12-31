c2f7f99b158ec04dcb9ab0f539fec87d23cb707e
Joshua Tang <joshuaystang@gmail.com>
2022-12-31 11:57:20 +1100

    feat: add to delete unknown callback query

--- tests/error/test_error_service.py
+++ tests/error/test_error_service.py
@@ -21,15 +21,31 @@ class TestErrorService(LanguageServiceTestMixin, TelegramTestMixin):
 
         self.telegram_callback_query.answer.assert_called_once()
         self.telegram_callback_query.edit_message_text.assert_called_once()
+        self.telegram_callback_query.delete_message.assert_not_called()
+        self.telegram_update.effective_message.reply_text.assert_not_called()
 
     @pytest.mark.asyncio
-    async def test_process_unknown_callback_query_bad_request(self) -> None:
+    async def test_process_unknown_callback_query_edit_bad_request(self) -> None:
         self.telegram_callback_query.edit_message_text.side_effect = BadRequest("Error")
+        await self.sut.process_unknown_callback_query(
+            self.telegram_update, self.telegram_context
+        )
+        self._assert_edit_delete_reply_text()
+
+    @pytest.mark.asyncio
+    async def test_process_unknown_callback_query_edit_delete_bad_request(self) -> None:
+        err = BadRequest("Error")
+        self.telegram_callback_query.edit_message_text.side_effect = err
+        self.telegram_callback_query.delete_message.side_effect = err
 
         await self.sut.process_unknown_callback_query(
             self.telegram_update, self.telegram_context
         )
 
+        self._assert_edit_delete_reply_text()
+
+    def _assert_edit_delete_reply_text(self) -> None:
         self.telegram_callback_query.answer.assert_called_once()
         self.telegram_callback_query.edit_message_text.assert_called_once()
+        self.telegram_callback_query.delete_message.assert_called_once()
         self.telegram_update.effective_message.reply_text.assert_called_once()
