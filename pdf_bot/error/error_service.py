893f9506e98f7d6c7497efc9cb91bb9618e7e12a
Joshua Tang <joshuaystang@gmail.com>
2022-12-31 19:50:14 +1100

    feat: update unknown callback query handler to only answer and reply text (#1309)

--- pdf_bot/error/error_service.py
+++ pdf_bot/error/error_service.py
@@ -1,5 +1,6 @@
-from telegram import Update
-from telegram.error import BadRequest
+from typing import cast
+
+from telegram import Message, Update
 from telegram.ext import ContextTypes
 
 from pdf_bot.language import LanguageService
@@ -12,17 +13,11 @@ class ErrorService:
     async def process_unknown_callback_query(
         self, update: Update, context: ContextTypes.DEFAULT_TYPE
     ) -> None:
-        query = update.callback_query
-        await query.answer()
-
         _ = self.language_service.set_app_language(update, context)
-        err_text = _("The button has expired, start over with your file or command")
+        text = _("The button has expired, start over with your file or command")
 
-        try:
-            await query.edit_message_text(err_text)
-        except BadRequest:
-            try:
-                await query.delete_message()
-            except BadRequest:
-                pass
-            await update.effective_message.reply_text(err_text)  # type: ignore
+        query = update.callback_query
+        await query.answer(text)
+
+        message = cast(Message, update.effective_message)
+        await message.reply_text(text)
