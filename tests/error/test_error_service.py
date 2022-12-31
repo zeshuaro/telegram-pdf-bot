453e9ce1fe683a6d4d1f38b8009dbc665e2a3b69
Joshua Tang <joshuaystang@gmail.com>
2022-12-31 19:47:42 +1100

    feat: update unknown callback query handler to only answer and reply text

--- tests/error/test_error_service.py
+++ tests/error/test_error_service.py
@@ -1,5 +1,4 @@
 import pytest
-from telegram.error import BadRequest
 
 from pdf_bot.error import ErrorService
 from tests.language import LanguageServiceTestMixin
@@ -18,28 +17,4 @@ class TestErrorService(LanguageServiceTestMixin, TelegramTestMixin):
         await self.sut.process_unknown_callback_query(self.telegram_update, self.telegram_context)
 
         self.telegram_callback_query.answer.assert_called_once()
-        self.telegram_callback_query.edit_message_text.assert_called_once()
-        self.telegram_callback_query.delete_message.assert_not_called()
-        self.telegram_update.effective_message.reply_text.assert_not_called()
-
-    @pytest.mark.asyncio
-    async def test_process_unknown_callback_query_edit_bad_request(self) -> None:
-        self.telegram_callback_query.edit_message_text.side_effect = BadRequest("Error")
-        await self.sut.process_unknown_callback_query(self.telegram_update, self.telegram_context)
-        self._assert_edit_delete_reply_text()
-
-    @pytest.mark.asyncio
-    async def test_process_unknown_callback_query_edit_delete_bad_request(self) -> None:
-        err = BadRequest("Error")
-        self.telegram_callback_query.edit_message_text.side_effect = err
-        self.telegram_callback_query.delete_message.side_effect = err
-
-        await self.sut.process_unknown_callback_query(self.telegram_update, self.telegram_context)
-
-        self._assert_edit_delete_reply_text()
-
-    def _assert_edit_delete_reply_text(self) -> None:
-        self.telegram_callback_query.answer.assert_called_once()
-        self.telegram_callback_query.edit_message_text.assert_called_once()
-        self.telegram_callback_query.delete_message.assert_called_once()
         self.telegram_update.effective_message.reply_text.assert_called_once()
