ae0b38fb9bac55794ae8e7fb9acddea1fe12fb7d
Joshua Tang <joshuaystang@gmail.com>
2023-02-08 21:19:45 +1100

    chore: update ruff noqa configs

--- pdf_bot/__main__.py
+++ pdf_bot/__main__.py
@@ -33,7 +33,7 @@ def main(
 
     if settings.app_url is not None:
         telegram_app.run_webhook(
-            listen="0.0.0.0",
+            listen="0.0.0.0",  # noqa: hardcoded-bind-all-interfaces
             port=settings.port,
             url_path=settings.telegram_token,
             webhook_url=f"{settings.app_url}/{settings.telegram_token}",
