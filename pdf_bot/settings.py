116bc114ac6c21a6a7b24184e4c1fea56ac67550
Joshua Tang <joshuaystang@gmail.com>
2023-11-13 23:00:32 +1100

    fix: remove unused settings field parameter

--- pdf_bot/settings.py
+++ pdf_bot/settings.py
@@ -9,18 +9,18 @@ class Settings(BaseSettings):
         extra="ignore", env_file=[Path(".env.test"), Path(".env")], env_file_encoding="utf-8"
     )
 
-    telegram_token: str = Field(..., env="telegram_token")
-    slack_token: str = Field(..., env="slack_token")
-    stripe_token: str = Field(..., env="stripe_token")
-    google_fonts_token: str = Field(..., env="google_fonts_token")
-    ga_api_secret: str = Field(..., env="ga_api_secret")
-    ga_measurement_id: str = Field(..., env="ga_measurement_id")
-    gcp_service_account: dict = Field(..., env="gcp_service_account")
+    telegram_token: str = Field(...)
+    slack_token: str = Field(...)
+    stripe_token: str = Field(...)
+    google_fonts_token: str = Field(...)
+    ga_api_secret: str = Field(...)
+    ga_measurement_id: str = Field(...)
+    gcp_service_account: dict = Field(...)
 
-    admin_telegram_id: int = Field(..., env="admin_telegram_id")
+    admin_telegram_id: int = Field(...)
 
-    app_url: str | None = Field(default=None, env="app_url")
-    port: int = Field(default=8443, env="port")
+    app_url: str | None = Field(default=None)
+    port: int = Field(default=8443)
 
     request_connection_pool_size: int = 8
     request_read_timeout: int = 45
@@ -28,4 +28,4 @@ class Settings(BaseSettings):
     request_connect_timeout: int = 45
     request_pool_timeout: int = 45
 
-    sentry_dsn: str | None = Field(default=None, env="sentry_dsn")
+    sentry_dsn: str | None = Field(default=None)
